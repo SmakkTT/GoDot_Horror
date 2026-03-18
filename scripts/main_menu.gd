93d278a0547dd78b1ba7e5cf83deaa4107cebcd4
kleins <mra.r4ik1@gmail.com>
2026-03-18 11:01:24 +0100

    en hel masse lort

---
+++ scripts/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/mainScene.tscn")
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	# This closes the application
+	get_tree().quit()
